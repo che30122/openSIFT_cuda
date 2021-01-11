577016f9a33952bdf3dc70d582cbe1406c78577f
che30122 <che30122@gmail.com>
2021-01-11 19:06:36 +0800

    fix main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,14 @@ extern void guassain_3conv(const Mat*,Mat*,double/*,int,int,int,int*/);
 int main(int argc,char** argv){
 	Mat img=imread(argv[1]);
 	Mat result=imread(argv[1]);
-//	guassain_3conv(&img,&result,atof(argv[3]),atoi(argv[4]),atoi(argv[5]),atoi(argv[6]),atoi(argv[7]));
-	guassain_3conv(&img,&result,atof(argv[3]));
+	double sigma=0.0,input_sigma;
+	input_sigma=atof(argv[3]);
+	if(input_sigma<=0 || input_sigma>=3.09){
+		sigma=3.09;
+	}
+	else
+		sigma=input_sigma;
+	guassain_3conv(&img,&result,sigma);
 	imwrite(argv[2],result);
 	return 0;
 }
